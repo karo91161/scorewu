a9fe98ed3ad27190660493849829f3bf7134fb3c
kalapacsrobert <robert.kalapacs@rabit.hu>
2024-08-10 19:31:43 +0200

    SWU-12
    - extended Teams menu to handle user's favorite teams (list, update, remove favorite teams)

--- src/services/leagueService.js
+++ src/services/leagueService.js
@@ -1,8 +1,10 @@
 // services/leagueService.js
 
 import axios from 'axios';
+import { getUser } from '../utils/userStorage';
 
 const API_URL = 'http://localhost:3000';
+const USER = getUser();
 
 export async function fetchFixtures(page = 1, limit = 50) {
   try {
@@ -39,3 +41,47 @@ export async function fetchTeams(league, season) {
     throw error;
   }
 }
+
+export async function fetchFavoriteTeams() {
+  console.log('USER', USER)
+  try {
+    const response = await axios.get(`${API_URL}/favorites`, {
+      params: {
+        userId: USER.id
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching favorite teams:', error);
+    throw error;
+  }
+}
+
+export async function addFavoriteTeam(teamId) {
+  try {
+    const response = await axios.post(`${API_URL}/favorites`, {
+      teamId,
+      userId: USER.id
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error adding favorite team:', error);
+    throw error;
+  }
+}
+
+export async function removeFavoriteTeam(teamId) {
+  try {
+    const response = await axios.delete(`${API_URL}/favorites`, {
+      data: {
+        teamId,
+        userId: USER.id
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error removing favorite team:', error);
+    throw error;
+  }
+}
+
